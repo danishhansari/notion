58a9c55886de6c37e7ab7d0f93c5a0e68f7ed8b2
Danish <dan71ish@gmail.com>
2024-07-28 15:16:37 +0530

    hero section done

---
+++ app/(marketing)/_components/hero.tsx
@@ -0,0 +1,4 @@
+"use client";
+export const Heroes = () => {
+  return <div>Hero</div>;
+};
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/hero";
+
+const MarketingPage = () => {
+  return (
+    <>
+      <div className='min-h-full flex flex-col'>
+        <div className='flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10'>
+          <Heading />
+          <Heroes />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MarketingPage;
