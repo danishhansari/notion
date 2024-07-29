ec3a37a73919e7988a98dac3e496005f01b1b270
Danish <dan71ish@gmail.com>
2024-07-29 09:49:43 +0530

    working on hero section image

--- app/(marketing)/_components/hero.tsx
+++ app/(marketing)/_components/hero.tsx
@@ -1,4 +1,28 @@
 "use client";
+
+import Image from "next/image";
+
 export const Heroes = () => {
-  return <div>Hero</div>;
+  return (
+    <div className='flex flex-col items-center justify-center max-w-5xl'>
+      <div className='flex items-center'>
+        <div className='relative max-w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]'>
+          <Image
+            src={"/hero0.png"}
+            fill
+            alt='Documents'
+            className='object-contain'
+          />
+        </div>
+        <div className='relative h-[400px] w-[400px] hidden md:block'>
+          <Image
+            src={"/hero3.png"}
+            fill
+            alt='Document'
+            className='object-contain'
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/hero";
 
@@ -5,10 +6,11 @@ const MarketingPage = () => {
   return (
     <>
       <div className='min-h-full flex flex-col'>
-        <div className='flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10'>
+        <div className='flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-4 md:px-6 pb-10'>
           <Heading />
           <Heroes />
         </div>
+        <Footer />
       </div>
     </>
   );
