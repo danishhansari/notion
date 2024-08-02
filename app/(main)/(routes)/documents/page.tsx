11bf907a981691b6d3cbd9d93e59b6d1c5996d65
Danish <dan71ish@gmail.com>
2024-08-03 00:38:58 +0530

    added the user profile in sidebar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,26 @@
-const DocumentsPage = () => {
-  return (
-    <div>DocumentsPage</div>
-  )
-}
+"use client";
+import Image from "next/image";
 
-export default DocumentsPage
+import { PlusCircle } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  const { user } = useUser();
+  const random = Math.floor(Math.random() * 9 + 1);
+  return (
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+      <Image src={`/hero${random}.png`} alt='Logo' height={300} width={300} />
+      <h2 className='text-lg font-medium'>
+        Welcome to {user?.firstName}&apos;s Notion
+      </h2>
+      <Button>
+        <PlusCircle className='h-4 w-4 mr-2' />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
