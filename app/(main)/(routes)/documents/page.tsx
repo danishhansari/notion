e8046527ed6574cb6255a924bf43d7d109e79028
Danish <dan71ish@gmail.com>
2024-08-03 09:30:17 +0530

    added the schema for user to create a file

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -5,9 +5,24 @@ import { PlusCircle } from "lucide-react";
 import { useUser } from "@clerk/clerk-react";
 
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Create a new node",
+      success: "New note created",
+      error: "Failed to create a new node",
+    });
+  };
+
   const random = Math.floor(Math.random() * 9 + 1);
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
@@ -15,7 +30,7 @@ const DocumentsPage = () => {
       <h2 className='text-lg font-medium'>
         Welcome to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className='h-4 w-4 mr-2' />
         Create a note
       </Button>
